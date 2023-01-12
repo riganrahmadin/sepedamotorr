d8360f2f5e4f26ebd8026911a224efe746929233
riganrahmadin <riganrahmadin8@gmail.com>
2023-01-13 02:39:36 +0700

    add finishing

--- application/controllers/admin/dashboard_admin.php
+++ application/controllers/admin/dashboard_admin.php
@@ -7,7 +7,7 @@ class Dashboard_admin extends CI_Controller{
         parent::__construct();
         if($this->session->userdata('role_id') !='1'){
             $this->session->set_flashdata('pesan','<div class="alert alert-danger alert-dismissible fade show" role="alert">
-               Anda belmum Login
+               Anda belmu Login
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
